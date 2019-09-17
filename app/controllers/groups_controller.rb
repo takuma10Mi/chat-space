e05a3a2d8204ea7d52987209be1afd7f0749bbe5
takuma10Mi <takumaii@yahoo.co.jp>
2019-09-17 22:12:51 +0900

    9.17夜

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  
+
   def index
   end
   def new
@@ -15,9 +15,21 @@ class GroupsController < ApplicationController
       render :new
     end
   end
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
+  end
 
   private
+
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
