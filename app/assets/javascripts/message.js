f16f61985ec2b99aedc208c1ded870c833dd3136
takuma10Mi <takumaii@yahoo.co.jp>
2019-10-19 15:54:56 +0900

    三項演算子使用
    修正しました。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,25 @@
 $(function(){ 
   function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.date}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.date}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
- }
+    var enzansi = ( message.image === null) ? `</div></div>`: `</div>
+    <img src=${message.image} >
+  </div>`; 
+
+    var html = `<div class="message" data-message-id=${message.id}>
+                <div class="upper-message">
+                  <div class="upper-message__user-name">
+                    ${message.user_name}
+                  </div>
+                  <div class="upper-message__date">
+                    ${message.date}
+                  </div>
+                </div>
+                <div class="lower-message">
+                  <p class="lower-message__content">
+                    ${message.content}
+                  </p>
+                  ${enzansi}`
+  return html;
+  }
 $('.js-form').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
@@ -61,5 +42,8 @@ $('.js-form').on('submit', function(e){
      alert('error');
    });
    return false;
+   
  });
+ 
+ 
 });
