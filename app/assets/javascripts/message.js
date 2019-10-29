7493ea3beeae4494722c0a40188978bb4ded4ebb
takuma10Mi <takumaii@yahoo.co.jp>
2019-10-29 22:56:06 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,47 +1,66 @@
-$(function(){ 
-  function buildHTML(message){
-    var messageimage = ( message.image === null) ? `</div></div>`: `</div>
-    <img src=${message.image} >
-  </div>`; 
-    var html = `<div class="message" data-message-id=${message.id}>
-                <div class="upper-message">
-                  <div class="upper-message__user-name">
-                    ${message.user_name}
-                  </div>
-                  <div class="upper-message__date">
-                    ${message.date}
-                  </div>
-                </div>
-                <div class="lower-message">
-                  <p class="lower-message__content">
-                    ${message.content}
-                  </p>
-                  ${messageimage}`
-  return html;
-  }
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formdata = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: 'POST',
-      data: formdata,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $("form")[0].reset();
-    })
-    .fail(function(data){
-      alert('エラーが発生したためメッセージは送信できませんでした。');
-    })
-    .always(function(data){
-      $('.contents__messages').animate({scrollTop: $('.contents__messages')[0].scrollHeight}, 'fast')
-      $('.contents__btn__form__box__submit').prop('disabled', false);
-    })  
-  })
-});
+$(document).on('turbolinks:load', function(){
+  $(function() {
+  
+    var search_list = $("#user-search-result");
+  
+    function appendUser(user){
+      var html = `<div class="chat-group-user clearfix">
+                    <p class="chat-group-user__name">${user.name}</p>
+                    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+                  </div>`;
+        search_list.append(html);
+    }
+  
+    function appendNoUser(user){
+      var html = `<div class='chat-group-user clearfix'>${ user }</div>`
+      search_list.append(html);
+    }
+  
+    var member_list = $("#chat-group-users");
+  
+    function addUser(userId,userName) {
+    var html = `<div id='chat-group-users'>
+                  <div class='chat-group-user clearfix js-chat-member' id='${userId}'>
+                    <input name='group[user_ids][]' type='hidden' value='${userId}'>
+                      <p class='chat-group-user__name'>${userName}</p>
+                      <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`;
+    member_list.append(html);
+      }
+  
+    $("#user-search-field").on("keyup", function() {
+      var input = $("#user-search-field").val();
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: { keyword: input },
+        dataType: 'json'
+      })
+      .done(function(users){
+        $("#user-search-result").empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            appendUser(user);
+          });
+        }
+        else {
+          appendNoUser("一致するユーザはいません");
+        }
+      })
+      .fail(function(){
+        alert('検索に失敗しました');
+      })
+    });
+      $(document).on("click", ".user-search-add", function () {
+      $('#chat-group-users').val();
+        var userId = $(this).data('user-id');
+        var userName = $(this).data('user-name');
+        addUser(userId,userName);
+        $(this).parent().remove();
+      });
+  
+      $(document).on("click", ".user-search-remove", function () {
+        $(this).parent().remove();
+      });
+    });
+  });
